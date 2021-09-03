a68df5e257e9d993b4612b0375ecd99df009bbb8
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2021-09-03 10:22:14 +0200

    player movement,ball movemen, inculding random starting direction

--- Game.js
+++ Game.js
@@ -1,12 +1,52 @@
-let player1 = new Player(10,350,40,40)
-let player2 = new Player(())
+let player1 = new Player(10,330,31,140)
+let player2 = new Player(859,330,31,140)
+let ball = new Ball(430,390,5,5,32,32)
+let scoreForP1;
+let scoreForP2;
+let isGameInProgress= true;
+
+
 // laver baggrund og sætter spillet til 60 frames pr sek
 function setup(){
 frameRate(60);
-let canvas =createCanvas (900,600);
+let canvas =createCanvas (900,700);
 background('black');
+ball.xspeed*=random([-1,1]);
+ball.yspeed*=random([-1,1]);
 };
+
 function draw(){
- player1.tegn();
- player2.tegn();
+    background('black');
+if(isGameInProgress){
+    player1.tegn();
+    player2.tegn();
+    ball.tegnBall();
+    ball.moveball();
+    if(keyIsDown(87)){
+       player1.movePlayer1(-5)
+    };
+    if(keyIsDown(83)){
+       player1.movePlayer1(5) 
+    };
+    if(keyIsDown(UP_ARROW)){
+       player2.movePlayer2(-5)
+    };
+    if(keyIsDown(DOWN_ARROW)){
+       player2.movePlayer2(5)
+    };
+    if(player1.y<=0){
+        player1.movePlayer1(10);
+    };
+    if(player1.y>=700-140){
+        player1.movePlayer1(-10);
+    };
+    if(player2.y<=0){
+        player2.movePlayer2(10);
+    };
+    if(player2.y>=700-140){
+        player2.movePlayer2(-10);
+    };
+ 
+} 
 };
+
--- players.js
+++ players.js
@@ -1,4 +1,4 @@
- class Player{
+class Player{
     constructor(xa, ya, wit, hei){
 this.x=xa;
 this.y=ya;
@@ -8,19 +8,34 @@ this.speedy=10;
     }
 
 tegn(){// Tegner spillerne
-    fill('white')
+    fill('white');
     rect(this.x,this.y,this.with,this.height)
     }
 // disse metoder bruges af game.js til at får spillerne til at flyttes sig
-movePlayer1(){
-
+movePlayer1(inp){
+this.y +=(inp);
 }
-movePlayer2(){
+movePlayer2(inp2){
+this.y +=(inp2);
+}};
 
+class Ball{
+    constructor(xp,yp,xs,ys,wit2,hei2){
+    this.xpos=xp;
+    this.ypos=yp;
+    this.xspeed=xs;
+    this.yspeed=ys;
+    this.with2=wit2;
+    this.height2=hei2;
+
+} 
+tegnBall(){
+    fill('white');
+    ellipse(this.xpos,this.ypos,this.with2,this.height2)
 }
-
-
-
+moveball(){
+this.xpos+=this.xspeed;
+this.ypos+=this.yspeed;
+}
 
 };
-
